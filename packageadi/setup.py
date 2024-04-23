77bc352684c9253732706ac8e848dadf6a905d82
x18198988 <159906039+x18198988@users.noreply.github.com>
2024-04-23 17:45:41 +0530

    Add files via upload

---
+++ packageadi/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+setup(name="packageaditya",
+      version="0.1",
+      description="Aditya Package",
+      author="Aditya",
+      packages=['packageaditya'],
+      install_requires=['datetime', 'random2'])
